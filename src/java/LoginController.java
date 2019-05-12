a1f9aee73cae4d0329ec065f9682d73e84559302
shubham <shubham@192.168.2.150>
2019-05-12 12:14:59 +0530

    weblogin

---
+++ src/java/LoginController.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String un=request.getParameter("username");
+		String pw=request.getParameter("password");
+		
+		// Connect to mysql and verify username password
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		 // loads driver
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/login", "root", ""); // gets a new connection
+ 
+		PreparedStatement ps = c.prepareStatement("select * from lo where ID=? and Password=?");
+		ps.setString(1, un);
+		ps.setString(2, pw);
+ 
+		ResultSet rs = ps.executeQuery();
+ 
+		while (rs.next()) {
+			response.sendRedirect("succes.html");
+			return;
+		}
+		response.sendRedirect("index.html");
+                
+		return;
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
